28d8299d953a6eb29988bbea2281a33f52d49a33
Michael Kamau <tokamau@gmail.com>
2016-10-10 22:26:58 +0300

    Add test - occurrence of word

---
+++ test/wordcounttests.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestWordCounts(unittest.TestCase):
+    """
+        Counts the occurrences or characters in a word
+    """
+
+    def test_word_occurance1(self):
+        self.assertDictEqual(
+            {'word': 1},
+            words('word'),
+            msg='should count one word'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()

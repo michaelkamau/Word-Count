3beb362765c37bf7061c81b15842bbf80579ad29
Michael Kamau <tokamau@gmail.com>
2016-10-10 23:32:56 +0300

    Update definition to handle int values

--- word_count/count.py
+++ word_count/count.py
@@ -1,5 +1,12 @@
 def words(str_words):
     word_list = str_words.split(' ')
+    # Check for strings with ints, convert if necessary
+    for i in range(len(word_list)):
+        try:
+            word_list[i] = int(word_list[i])
+        except ValueError:
+            continue
+
     word_count = {}
     for w in word_list:
         word_count.setdefault(w, 0)

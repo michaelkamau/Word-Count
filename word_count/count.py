d3e3dbb9c947454a49711ef9b118ad6eee23d3cb
Michael Kamau <tokamau@gmail.com>
2016-10-10 22:56:47 +0300

    Update definition to support multiple word occurrences

--- word_count/count.py
+++ word_count/count.py
@@ -1,3 +1,7 @@
 def words(str_words):
     word_list = str_words.split(' ')
-    return {w : 1 for w in word_list}
+    word_count = {}
+    for w in word_list:
+        word_count.setdefault(w, 0)
+        word_count[w] += 1
+    return word_count
